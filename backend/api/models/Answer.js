50430debe91215840fddaa32567112b8bd980a20
陳信屹 <ossug.hychen@gmail.com>
2015-03-31 14:30:24 +0800

    add Answer.stats.

--- backend/api/models/Answer.js
+++ backend/api/models/Answer.js
@@ -4,6 +4,7 @@
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
+_ = require('underscore');
 
 module.exports = {
 
@@ -20,6 +21,25 @@ module.exports = {
       type: 'json',
       required: true
     }
+  },
+
+  stats: function(cb){
+    Answer.find().exec(function(err, answers){
+      if(err) return cb(err);
+      var result = {};
+      _.map(result, function(e){
+        var count = function(id, cat){
+          if(!result[id]){
+            result[id] = {agree:0, disagree:0, neutral:0};
+          }
+          result[id][cat] += 1;
+        }
+        _.map(e.agree, function(id){ return count(id, 'agree');});
+        _.map(e.neutral, function(id){ return count(id, 'neutral');});
+        _.map(e.disagree, function(id){ return count(id, 'disagree');});
+      });
+      cb(null, result);
+    });
   }
 };
 
