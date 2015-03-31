63e44c7bf1d56db9f09983e8bea07b1b5d639783
陳信屹 <ossug.hychen@gmail.com>
2015-03-31 15:50:39 +0800

    add Answer fixture and Answer.stats asertion.

--- backend/test/unit/models/Answer.js
+++ backend/test/unit/models/Answer.js
@@ -5,4 +5,27 @@ describe('Answers', function(){
       done();
     });
   });
+  describe('#stats()', function(){
+    before(function(done){
+      var Barrels = require('barrels');
+      var barrels = new Barrels();
+      var fixtures = barrels.data;
+      barrels.populate(['answer'], function(err){
+        done();
+      });
+    });
+    it('should return statsitic.', function(done){
+      Answer.stats(function(err, stats){
+        expected = {
+          '1': { agree: 1, disagree: 0, neutral: 0 },
+          '2': { agree: 1, disagree: 0, neutral: 0 },
+          '3': { agree: 0, disagree: 0, neutral: 1 },
+          '4': { agree: 0, disagree: 0, neutral: 1 },
+          '5': { agree: 0, disagree: 1, neutral: 0 }
+        }
+        expected.should.deep.eq(stats);
+        done();
+      });
+    });
+  });
 });
